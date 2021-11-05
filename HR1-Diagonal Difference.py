3eeb2a074e940dea5d634b01436956a8dc657e36
Prasad Dalwee <54315571+PrasadDalwee@users.noreply.github.com>
2021-11-05 00:22:01 -0700

    Create HR1-Diagonal Difference.py
    https://www.hackerrank.com/challenges/one-month-preparation-kit-diagonal-difference/problem?h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-one

---
+++ HR1-Diagonal Difference.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    x=len(arr[0])
+    i=0
+    s1=0
+    s2=0
+    while i<x:
+        s1+=arr[i][i]
+        s2+=arr[x-1-i][i]
+        i+=1
+    
+    return abs(s1-s2)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
