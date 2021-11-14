826d081102f6ad8a65cb18e316e4d09acb8ab999
Prasad Dalwee <54315571+PrasadDalwee@users.noreply.github.com>
2021-11-14 06:57:07 -0800

    Create HR2-Sherlock and Array.py
    https://www.hackerrank.com/challenges/one-month-preparation-kit-sherlock-and-array/problem?h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-two

---
+++ HR2-Sherlock and Array.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'balancedSums' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def balancedSums(arr):
+    # Write your code here
+    rsum=0
+    lsum=0
+    for i in arr:
+        rsum+=i
+    for i in arr:
+        rsum-=i
+        if rsum==lsum:
+            return "YES"
+        lsum+=i
+    return "NO"
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    T = int(input().strip())
+
+    for T_itr in range(T):
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = balancedSums(arr)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
