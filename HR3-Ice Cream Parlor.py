9f61d89bfd7b9fd38ce39eab74905939ec800812
Prasad Dalwee <54315571+PrasadDalwee@users.noreply.github.com>
2021-12-02 05:07:12 -0800

    Question link->
    https://www.hackerrank.com/challenges/one-month-preparation-kit-icecream-parlor/problem?h_l=interview&isFullScreen=false&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-three

---
+++ HR3-Ice Cream Parlor.py
@@ -0,0 +1,47 @@
+            # O(n) solution
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'icecreamParlor' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER m
+#  2. INTEGER_ARRAY arr
+#
+
+def icecreamParlor(m, arr):
+    # Write your code here
+    diff={}
+    for i in range(len(arr)):
+        diff[m-arr[i]]=i
+    
+    for i in range(len(arr)):
+        if arr[i] in diff and diff[arr[i]]!=i:
+            return [i+1,diff[arr[i]]+1]
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        m = int(input().strip())
+
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = icecreamParlor(m, arr)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
