554732dbb021f2c3f656148d7267c88065b36193
PrasadDalwee <prasaddalwee@gmail.com>
2021-12-27 18:10:17 +0530

    dec27

---
+++ Leetcode/Dec21-Monthly Challenge/c.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    
+    int findComplement(int num) 
+    {
+        int x=0,ans=0;
+        while(num)
+        {
+            if((num&1)==0)
+                ans+=1<<x;//left shift 1 x times to get 1+10^x and add to ans 
+            
+            num>>=1;//right shift num by 1 to remove last bit or LSB
+            x++;
+        }
+
+        return ans;
+
+    }
+};
+
+int main()
+{
+    int n;
+    cout<<"\nEnter no.: ";
+    cin>>n;
+    Solution s;
+    cout<<endl<<"1's complement: "<<s.findComplement(n);
+    
+}
