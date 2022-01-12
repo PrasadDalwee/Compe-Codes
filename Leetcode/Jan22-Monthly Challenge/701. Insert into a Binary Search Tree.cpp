ddc420c6c79aed535f426296e3ebf63803a8ac0f
PrasadDalwee <prasaddalwee@gmail.com>
2022-01-12 09:48:16 +0530

    jan11

---
+++ Leetcode/Jan22-Monthly Challenge/701. Insert into a Binary Search Tree.cpp
@@ -0,0 +1,37 @@
+//  link:https://leetcode.com/problems/insert-into-a-binary-search-tree/
+//  Topic: Binary Search Tree
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) 
+    {
+        if(!root)
+            return new TreeNode(val);
+        
+        if(val>root->val)
+        {
+            if(!root->right)
+            {
+                root->right=new TreeNode(val);
+                return root;
+            }
+            insertIntoBST(root->right,val);
+        }
+        
+        else if(val<root->val)
+        {
+             if(!root->left)
+            {
+                root->left=new TreeNode(val);
+                return root;
+            }
+            insertIntoBST(root->left,val);
+        }
+        
+        return root;
+    }
+};
+
