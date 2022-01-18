eca140d5a01b52a3f9770720fea2a0c375f7b5d5
PrasadDalwee <prasaddalwee@gmail.com>
2022-01-18 09:37:51 +0530

    jan18

---
+++ Leetcode/Template.cpp
@@ -0,0 +1,19 @@
+//  link:
+//  Topic: 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution 
+{
+    public:
+        void Func()
+        {
+
+        }
+};
+
+int main()
+{
+    
+}
