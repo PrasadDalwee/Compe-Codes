6b96728380629e089b76a3802163ff864016d697
PrasadDalwee <prasaddalwee@gmail.com>
2022-01-22 10:12:25 +0530

    jan22

--- Leetcode/Jan22-Monthly Challenge/Template.cpp
+++ Leetcode/Jan22-Monthly Challenge/Template.cpp
@@ -1,5 +1,5 @@
-//  link:
-//  Topic: 
+//  link:https://leetcode.com/problems/gas-station/
+//  Topic: Arrays, Greedy
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -7,51 +7,21 @@ using namespace std;
 class Solution 
 {
     public:
-        bool travel(vector<int>& gas, int &start)
-        {
-            int sum=0, end=-1;
+        int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+            int n = gas.size();
+            int total_surplus = 0;
+            int surplus = 0;
+            int start = 0;
             
-            if(gas[start]<=0)
-                return 0;
-            
-            cout<<start<<endl;
-
-            end=start;
-            sum+=gas[end];
-            cout<<"\t"<<end<<" "<<sum<<endl;
-            end=(end+1)%gas.size();
-
-            while(end!=start)
-            {
-                sum+=gas[end];
-                cout<<"\t"<<end<<" "<<sum<<endl;
-                end=(end+1)%gas.size();
-                if(sum<0)
-                    return 0;
+            for(int i = 0; i < n; i++){
+                total_surplus += gas[i] - cost[i];
+                surplus += gas[i] - cost[i];
+                if(surplus < 0){
+                    surplus = 0;
+                    start = i + 1;
+                }
             }
-            return 1;
-        }
-    
-        int canCompleteCircuit(vector<int>& gas, vector<int>& cost) 
-        {
-            int sum=0;
-            for(int i=0;i<gas.size();i++)
-            {    
-                gas[i]-=cost[i];
-                sum+=gas[i];
-            }
-            cout<<endl<<sum<<endl;
-            
-
-            if(sum<0)
-                return -1;
-            
-            for(int start=0;start<gas.size();start++)
-                if(travel(gas,start))
-                    return start;
-            
-            return -1;
-            
+            return (total_surplus < 0) ? -1 : start;
         }
 };
 
