a08c2091fe9ecd0f4b6fd196d358a5755d5d336c
PrasadDalwee <prasaddalwee@gmail.com>
2022-02-16 21:47:19 +0530

    feb16

---
+++ Leetcode/2022/02 Feb22-Monthly Chalenge/24. Swap Nodes in Pairs.cpp
@@ -0,0 +1,48 @@
+//  link:
+//  Topic: 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // Definition for singly-linked list.
+ struct ListNode 
+ {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+
+
+class Solution 
+{
+    public:
+        ListNode* swapPairs(ListNode* head) 
+        {
+            ListNode *node, *GP, *nextnode;
+            
+            node=head;
+            if(node)
+                nextnode=node->next;
+            GP=NULL;
+            
+            while(node && nextnode)
+            {
+                if(!GP)
+                    head=nextnode;
+                else
+                    GP->next=nextnode;
+                
+                GP=node;
+                node->next=nextnode->next;
+                nextnode->next=node;
+                node=node->next;
+                if(node)
+                    nextnode=node->next;
+            }
+            
+            return head;
+        }
+};
